6de681bbb8def9e9bde7a070174f20bf90b9c4e4
jmeichelbeck <julien.meichelbeck@doctolib.com>
2021-12-31 15:56:18 +0100

    fix tests

--- app/models/user_buddy.rb
+++ app/models/user_buddy.rb
@@ -7,10 +7,11 @@ class UserBuddy < ApplicationRecord
   belongs_to :buddy, class_name: 'User'
 
   def self.generate_graph
-    RGL::DirectedAdjacencyGraph[*self.joins(:user, :buddy).pluck('users.email', 'buddies_user_buddies.email').flatten]
+    RGL::DirectedAdjacencyGraph[*self.joins(:user, :buddy).pluck(:user_id, :buddy_id).flatten]
   end
 
   def self.generate_visual_graph
-    generate_graph.write_to_graphic_file('png', 'public/friendship')
+    RGL::DirectedAdjacencyGraph[*self.joins(:user, :buddy).pluck('users.email', 'buddies_user_buddies.email').flatten]
+      .write_to_graphic_file('png', 'public/friendship')
   end
 end
