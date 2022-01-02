210be766b6971d17d83ac00a9f9cfb1ca1f9cd2b
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@doctolib.com>
2022-01-02 21:50:53 +0100

    Fix typo in migration

--- db/migrate/20211230170511_add_notification_token_to_users.rb
+++ db/migrate/20211230170511_add_notification_token_to_users.rb
@@ -1,5 +1,5 @@
 class AddNotificationTokenToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :notifcation_token, :string
+    add_column :users, :notification_token, :string
   end
 end
