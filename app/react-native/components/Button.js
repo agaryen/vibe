afb278955e928aa948195cc24361d2b585bfe52c
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@doctolib.com>
2022-01-02 19:33:54 +0100

    Add button component

---
+++ app/react-native/components/Button.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text, TouchableOpacity } from 'react-native';
+import tw from 'twrnc';
+
+const uiColors = {
+  primary: 'bg-indigo-500 text-white',
+  danger: 'bg-red-500 text-white',
+  success: 'bg-green-500 text-white',
+};
+
+const Button = ({ children, style, uiStyle = 'primary', onPress, disabled, fluid = false, ...props }) => {
+  const color = disabled ? 'bg-gray-300 text-white' : uiColors[uiStyle];
+
+  return (
+    <TouchableOpacity
+      {...props}
+      onPress={onPress}
+      style={[tw`px-4 py-3 rounded-lg`, tw.style(color), style]}
+      disabled={disabled}
+    >
+      <Text style={[tw`text-base font-semibold self-center`, tw.style(color)]}>{children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+Button.propTypes = {
+  uiStyle: PropTypes.oneOf(['primary', 'danger', 'success']),
+  fluid: PropTypes.bool,
+};
+
+export default Button;
