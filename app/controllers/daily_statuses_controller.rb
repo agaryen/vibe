954c8389e7c8f9e2f96619518df804bc0d194c0c
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@doctolib.com>
2022-01-03 01:05:14 +0100

    First draw of answer selection

--- app/controllers/daily_statuses_controller.rb
+++ app/controllers/daily_statuses_controller.rb
@@ -1,20 +1,32 @@
 class DailyStatusesController < ApplicationController
   before_action :set_daily_status
   before_action :authenticate_user!
+  skip_forgery_protection
 
   def show
     render json: @daily_status.to_json(only: %i(day answer))
   end
 
   def create
-    @daily_status = current_user.daily_statuses.find_or_initialize_by(day: params[:daily_status][:day])
-    @daily_status.answer = params[:daily_status][:answer]
-    @daily_status.save!
-    redirect_to root_path(day: @daily_status.day)
+    respond_to do |format|
+      format.html do
+        @daily_status = current_user.daily_statuses.find_or_initialize_by(day: params[:daily_status][:day])
+        @daily_status.answer = params[:daily_status][:answer]
+        @daily_status.save!
+        redirect_to root_path(day: @daily_status.day)
+      end
+
+      format.json do
+        @daily_status.answer = params[:daily_status][:answer]
+        @daily_status.save!
+        head :created
+      end
+    end
   end
 
   private
 
+  # TODO: Messy initialization and creation. IMO, we should create the record before the user even answered, and then just fill the answer column
   def set_daily_status
     @daily_status = current_user.daily_statuses.find_or_initialize_by(day: 1.day.from_now)
   end
--- app/react-native/screens/DailyStatus/DailyStatus.js
+++ app/react-native/screens/DailyStatus/DailyStatus.js
@@ -1,19 +1,74 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import tw from 'twrnc';
-import { Text, View, SafeAreaView, KeyboardAvoidingView } from 'react-native';
+import { Text, View, SafeAreaView } from 'react-native';
 import Button from 'vibe/components/Button';
-import TextInput from 'vibe/components/TextInput';
 import useHttpQuery from 'vibe/modules/hooks/useHttpQuery';
 
+const choicesOfAnswer = {
+  NO: "I won't be present",
+  MAYBE: "Meh. I'll be there if my buddies are there",
+  YES: "I'll be there for sure",
+};
+
 const DailyStatus = ({ setIsAuthenticated }) => {
-  useHttpQuery({
+  const {
+    loading,
+    data: { answer } = {},
+    triggerQuery: triggerFetch,
+  } = useHttpQuery({
     url: '/daily_statuses',
     onFailure: ({ status }) => {
       if (status === 401) setIsAuthenticated(false);
     },
+    trigger: true,
+  });
+  const { triggerQuery: triggerAnswer } = useHttpQuery({
+    url: '/daily_statuses',
+    method: 'POST',
+    trigger: true,
+    onSuccess: () => triggerFetch(),
   });
 
-  return <></>;
+  const chooseAnswer = (choice) => {
+    triggerAnswer({
+      body: {
+        daily_status: {
+          answer: choice,
+        },
+      },
+    });
+  };
+
+  useEffect(() => {
+    triggerFetch();
+  }, []);
+
+  if (loading) return null;
+  return (
+    <SafeAreaView style={tw`flex-1`}>
+      <View style={tw`flex h-full w-full px-8 justify-between items-center`}>
+        <Text style={tw`text-5xl text-center font-bold mt-8`}>Welcome</Text>
+        <View style={tw`w-full`}>
+          <Text style={tw`text-2xl font-bold mb-8`}>{"What's your office status tomorrow?"}</Text>
+          {answer === null ? (
+            <>
+              <Button uiStyle="danger" style={tw`mb-8`} onPress={() => chooseAnswer('NO')}>
+                {choicesOfAnswer.NO}
+              </Button>
+              <Button uiStyle="warning" style={tw`mb-8`} onPress={() => chooseAnswer('MAYBE')}>
+                {choicesOfAnswer.MAYBE}
+              </Button>
+              <Button uiStyle="success" onPress={() => chooseAnswer('YES')}>
+                {choicesOfAnswer.YES}
+              </Button>
+            </>
+          ) : (
+            <Text style={tw`text-xl mb-40`}>{`You answered "${choicesOfAnswer[answer]}"`}</Text>
+          )}
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default DailyStatus;
