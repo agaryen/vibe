0b7db97a6a6e9560466bfe91a34d0eb50572051e
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@doctolib.com>
2022-01-16 19:24:51 +0100

    Add convenience answer scope to daily status

--- app/models/daily_status.rb
+++ app/models/daily_status.rb
@@ -1,4 +1,8 @@
 
 class DailyStatus < ApplicationRecord
   belongs_to :user
+
+  scope :answered_yes, -> { where(status: Attendancy::YES) }
+  scope :answered_maybe, -> { where(status: Attendancy::MAYBE) }
+  scope :answered_no, -> { where(status: Attendancy::NO) }
 end
